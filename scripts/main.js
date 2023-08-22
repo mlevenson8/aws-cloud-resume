cc32dc6dd5e709715dfb33ccecc1c395cf25ec91
mlevenson8 <mlevenson88@gmail.com>
2023-08-22 00:32:04 -0500

    parse the js object to get visit_count number

--- scripts/main.js
+++ scripts/main.js
@@ -9,7 +9,7 @@ $(document).ready(() => {
   $.post('https://zfltx34xol.execute-api.us-east-1.amazonaws.com/Prod/visit')
   .done(visitor_counter => {
       $('#loader').hide();
-      $('#visits').text(visitor_counter);
+      $('#visits').text(Object.values(visitor_counter));
   })
   .fail(e => {
       console.log('Error');
